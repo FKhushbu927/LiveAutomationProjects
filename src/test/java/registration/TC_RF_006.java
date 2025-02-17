3f788e63640c1a07ea2ea1aa6faf18410643df09
FKhushbu927 <jannateekhushbu927@gmail.com>
2025-02-17 20:33:33 +0600

    TC_RF_006 test case automation completed

--- src/test/java/registration/TC_RF_006.java
+++ src/test/java/registration/TC_RF_006.java
@@ -1,16 +1,18 @@
 package registration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
 import java.time.Duration;
 import java.util.Date;
 
-public class TC_RF_005 {
+public class TC_RF_006 {
 
     @Test
-    public void verifyRegisterAccountBySubscribingWithNewsLetter() {
+    public void verifyRegisterAccountSayingNoToNewsLetter() {
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
@@ -25,32 +27,28 @@ public class TC_RF_005 {
         driver.findElement(By.id("input-telephone")).sendKeys("787655342");
         driver.findElement(By.id("input-password")).sendKeys("@#Khushbu1234");
         driver.findElement(By.id("input-confirm")).sendKeys("@#Khushbu1234");
-        driver.findElement(By.xpath("//input[@name='newsletter'][@value='1']")).click();
+
+        driver.findElement(By.xpath("//input[@name='newsletter'][@value='0']")).click();
+
         driver.findElement(By.name("agree")).click();
         driver.findElement(By.xpath("//input[@value='Continue']")).click();
-
         driver.findElement(By.xpath("//div[@class='buttons']//a[text()='Continue']")).click();
 
-
         driver.findElement(By.linkText("Subscribe / unsubscribe to newsletter")).click();
-
-
         Assert.assertTrue(driver.findElement(By.xpath("//ul[@class='breadcrumb']//a[text()='Newsletter']")).isDisplayed());
-        Assert.assertTrue(driver.findElement(By.xpath("//input[@name='newsletter'][@value='1']")).isSelected());
+        Assert.assertTrue(driver.findElement(By.xpath("//input[@name='newsletter'][@value='0']")).isSelected());
 
         driver.quit();
     }
 
+
     public String generateGmail() {
         Date date = new Date();
-        System.out.println(date);
         String stringDate = date.toString();
-        System.out.println(stringDate);
         String noSpaceStringDate = stringDate.replaceAll("\\s", "");
         String noColonStringDate = noSpaceStringDate.replaceAll("\\:", "");
         String lowercaseString = noColonStringDate.toLowerCase();
         String gmailWithTimeStamp = lowercaseString + "@gmail.com";
-        //System.out.println(gmailWithTimeStamp);
         return gmailWithTimeStamp;
     }
 }
